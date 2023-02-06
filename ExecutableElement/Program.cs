b62b86120fee58e629a2b63fdd91eeed4ed7d4ce
Locked15 <almgal353@gmail.com>
2023-02-06 20:31:52 +0500

    Added original test data for executable module.

--- ExecutableElement/Program.cs
+++ ExecutableElement/Program.cs
@@ -8,17 +8,23 @@ namespace Temp
         {
             var starts = new TimeSpan[]
             {
-                new TimeSpan(2, 30, 0),
-                new TimeSpan(4, 0, 0)
+                new(10, 00, 0),
+                new(11, 00, 0),
+                new(15, 00, 0),
+                new(15, 30, 0),
+                new(16, 50, 0)
             };
             var durations = new int[]
             {
+                60,
                 30,
-                60
+                10,
+                10,
+                40
             };
-            var begin = new TimeSpan(22, 0, 0);
-            var end = new TimeSpan(12, 0, 0);
-            var consultationTime = 40;
+            var begin = new TimeSpan(08, 00, 00);
+            var end = new TimeSpan(18, 00, 00);
+            var consultationTime = 30;
 
             var res = Calculations.AvailablePeriods(starts, durations, begin, end, consultationTime);
             res.ToString();
